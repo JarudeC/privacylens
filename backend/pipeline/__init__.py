88d291135a3d37629178b33d7fc538197b536169
wonnom <weesiang9@gmail.com>
2025-08-30 20:52:33 +0800

    edited innit

--- backend/pipeline/__init__.py
+++ backend/pipeline/__init__.py
@@ -1,10 +1,10 @@
-from extract import process_video
-from detect import detect_video
-from blur import blur_video 
+# from extract import process_video
+# from detect import detect_video
+# from blur import blur_video 
 
-results = detect_video("../assets/Clear test.MOV", "../models/Credit.pt")
-unique_results = process_video(results, "../assets/Clear test.MOV")
-# print(results)
+# results = detect_video("../assets/Clear test.MOV", "../models/Credit.pt")
+# unique_results = process_video(results, "../assets/Clear test.MOV")
+# # print(results)
 
-url = blur_video(results, "../processed/test_blurred.mp4", [2])
-print(unique_results)
+# url = blur_video(results, "../processed/test_blurred.mp4", [2])
+# print(unique_results)
